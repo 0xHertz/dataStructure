5059204ee6bd3b37908a0edb655d4b30bebcf43b
Liubing-Zhen <55642724+Liubing-Zhen@users.noreply.github.com>
2021-04-24 13:19:37 +0800

    re*9new

---
+++ prim.c
@@ -0,0 +1,16 @@
+# ifdef
+
+void Prim(G, T){
+    T = NULL;            //清空树
+    U = {w};             //加入任一节点
+    while ((V-U) != NULL))
+    {
+        //找到最小权值的边（u，v）
+        
+        //并入
+        T = T + (u, v);
+        U = U + v;
+    }
+    
+}
+# endif
